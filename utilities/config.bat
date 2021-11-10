60262ae0583173e3162479cf9c33e6bf9adaefde
RegularSpark <71980947+RegularSpark@users.noreply.github.com>
2021-11-09 22:17:34 -0500

    Update config.bat

--- utilities/config.bat
+++ utilities/config.bat
@@ -2,21 +2,13 @@ setlocal
 if "%SUBSCRIPT%"=="" ( start notepad.exe "%CD%\%~nx0" & exit )
 endlocal
 
-:: verbose
 set VERBOSEWRAPPER=n
 
-:: skip check depends
 set SKIPCHECKDEPENDS=n
 
-
-:: dry run
 set DRYRUN=n
 
 set DEVMODE=n
-
-
-:: headless mode
-set APPCHROMIUM=y
 
 set PORT=4343
 
