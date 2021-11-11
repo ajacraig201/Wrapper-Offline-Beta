fa08386d43140a763f4e4c824d7ae71b4170a6c9
RegularSpark <8034086-RegularSpark@users.noreply.gitlab.comgit>
2021-11-10 23:29:47 -0500

    fixed cc coloring (my bad)

--- wrapper/static/page.js
+++ wrapper/static/page.js
@@ -204,9 +204,9 @@ module.exports = function (req, res, url) {
 				}
 			}
 		</script>
-		<link rel="stylesheet" type="text/css" href="/pages/css/swf.css">
 		<link rel="stylesheet" type="text/css" href="/pages/css/modern-normalize.css">
 		<link rel="stylesheet" type="text/css" href="/pages/css/global.css">
+		<link rel="stylesheet" type="text/css" href="/pages/css/swf.css">
 	</head>
 	
 	<header id="header">
