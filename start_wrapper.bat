871141dfcdffb3eeca0a28dd6e01400622bbfd41
Spark <71980947+RegularSpark@users.noreply.github.com>
2022-01-10 19:11:30 -0500

    how about you stop being a FUCKING IDIOT

--- start_wrapper.bat
+++ start_wrapper.bat
@@ -13,9 +13,10 @@
 
 pushd "%~dp0"
 if exist .git (
-	echo Checking for updates...
-	call utilities\PortableGit\bin\git.exe fetch --all
 	echo Updating...
+	call utilities\PortableGit\bin\git.exe checkout main
+	call utilities\PortableGit\bin\git.exe branch backup
+	call utilities\PortableGit\bin\git.exe fetch --all
 	call utilities\PortableGit\bin\git.exe reset --hard origin/main
 	PING -n 3 127.0.0.1>nul
 	cls
