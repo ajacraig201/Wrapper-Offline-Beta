6884ccb3bd90cbb1755a6b28d01d8b2817af540e
Spark <71980947+RegularSpark@users.noreply.github.com>
2022-01-23 19:14:56 -0500

    Update main.js

--- wrapper/main.js
+++ wrapper/main.js
@@ -1,9 +1,2 @@
 // Core part of Wrapper: Offline
 require("./server");
-
-
-// Loads env.json for Wrapper version and build number
-const env = Object.assign(process.env,
-	require('./env'));
-// env.json variables
-let version = env.WRAPPER_VER;
