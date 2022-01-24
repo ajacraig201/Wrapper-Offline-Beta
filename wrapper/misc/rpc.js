a61e5fe1d56195cff078666a0a90f2d13e5ad570
Spark <71980947+RegularSpark@users.noreply.github.com>
2022-01-23 19:09:20 -0500

    Add files via upload

---
+++ wrapper/misc/rpc.js
@@ -0,0 +1,71 @@
+const RPC = require("discord-rpc");
+const http = require("http");
+
+// Loads env.json for Wrapper version and build number
+const env = Object.assign(process.env,
+	require('../env'));
+// env.json variables
+let version = env.WRAPPER_VER;
+
+
+// Discord rich presence
+const rpc = new RPC.Client({
+	transport: "ipc"
+});
+module.exports = {
+	setActivity(page) {
+		switch (page) {
+			case "vl": { 
+				var state = 'Video List'; 
+				break; 
+			}
+			case "vm": { 
+				var state = 'Making a Video'; 
+				break; 
+			}
+			case "cc": { 
+				var state = 'Creating a Character'; 
+				break; 
+			}
+			case "ccb": { 
+				var state = 'Browsing Characters'; 
+				break; 
+			}
+			case "vp": { 
+				var state = 'Watching a Video'; 
+				break; 
+			}
+		}
+		// Sets RPC activity
+		rpc.setActivity({
+			state: state,
+			details: "Version " + version,
+			startTimestamp: new Date(),
+			largeImageKey: "icon",
+			largeImageText: "Wrapper: Offline",
+			smallImageKey: "Wrapper: Offline",
+			smallImagetext: "Wrapper: Offline",
+		});
+	}
+}
+
+rpc.on("ready", () => {
+	rpc.setActivity({
+		state: 'Starting',
+		details: "Version " + version,
+		startTimestamp: new Date(),
+		largeImageKey: "icon",
+		largeImageText: "Wrapper: Offline",
+		smallImageKey: "Wrapper: Offline",
+		smallImagetext: "Wrapper: Offline",
+	});
+});
+// Connects RPC to app
+try {
+	rpc.login({
+		clientId: "866340172874383370"
+	});
+	console.log('Rich prescense is on!')
+} catch (e) {
+	console.log(e);
+}
