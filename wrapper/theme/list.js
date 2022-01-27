2060706c68bbd5f48b15b701d91e091e88774eaa
Spark <71980947+RegularSpark@users.noreply.github.com>
2022-01-27 18:01:27 -0500

    Add files via upload

--- wrapper/theme/list.js
+++ wrapper/theme/list.js
@@ -1,68 +1,12 @@
-/* Looking for a written form of the themes list?
-"action"
-"akon"
-"animal"
-"anime"
-"ben10"
-"bizmodels"
-"botdf"
-"bunny"
-"business"
-"cc_store"
-"chibi"
-"chowder"
-"christmas"
-"common"
-"commoncraft"
-"custom"
-"domo"
-"fullenergy"
-"infographics"
-"monkeytalk"
-"monstermsh"
-"ninja"
-"ninjaanime"
-"politic"
-"politics2"
-"retro"
-"sf"
-"space"
-"spacecitizen"
-"startrek"
-"stick"
-"sticklybiz"
-"street"
-"underdog"
-"vietnam"
-"whiteboard"
-"willie"
-*/
-
 const http = require("http");
 const fUtil = require("../misc/file");
 const folder = process.env.THEME_FOLDER;
 const fs = require("fs");
-const extrathemes = `
-	<theme id="toonadv" name="Toon Adventure" enable="N" />
-	<theme id="underdog" name="Saturday Morning TV" enable="N" />
-	<theme id="willie" name="Willie Nelson" enable="N" />
-	<theme id="underdog" name="Saturday Morning TV" enable="N" />
-	<theme id="willie" name="Willie Nelson" enable="N" />
-	<theme id="fullenergy" name="Full Energy" thumb=""/>
-	<theme id="akon" name="AKON" />
-	<theme id="ben10" name="Ben 10" />
-	<theme id="botdf" name="Blood on the Dance Floor" cc_theme_id="botdf" />
-	<theme id="bunny" name="It's Happy Bunny" enable="N" />
-	<theme id="chowder" name="Chowder" enable="N" />
-	<theme id="domo" name="Domo" enable="N" />
-	<theme id="monkeytalk" name="SuperRica &amp; Rashy" enable="N" />
-	<theme id="christmas" name="Holiday &amp; Seasonal" thumb="santa-stand.swf" />
-	<theme id="animal" name="Lil' Petz" thumb="" />
-	<theme id="bizmodels" name="Business Models" thumb="" />
-	<theme id="sticklybiz" name="Stickly Business" thumb="" />
-	<theme id="vietnam" name="Jungle Warfare" thumb="" />
-	<theme id="monstermsh" name="Monsters Mayhem" thumb="" />
-`;
+var themelist = fs.readFileSync(`${folder}/_themelist.xml`);
+
+if (process.env.TRUNCATE_THEMES == "n") {
+	var themelist = themelist.replace(/<!--/g, ``).replace(/-->/g, ``);
+}
 
 /**
  * @param {http.IncomingMessage} req
@@ -72,14 +16,21 @@ const extrathemes = `
  */
 
 module.exports = function (req, res, url) {
+	/*
 	if (req.method != "POST" || url.path != "/goapi/getThemeList/") return;
-	let buffer = fs.readFileSync(`${folder}/_themelist.xml`);
+	var buffer = fs.readFile(`${folder}/_themelist.xml`);
 	if (process.env.TRUNCATE_THEMES == "y") {
-		let buffer = buffer.replace(/THEMEEXTRA/g, ``);
+		var buffer = buffer.replace(/THEMEEXTRA/g, ``);
 	} else {
-		let buffer = buffer.replace(/THEMEEXTRA/g, `\n${extrathemes}`);
+		var extrathemes = `<theme id="toonadv" name="Toon Adventure" enable="N" /><theme id="underdog" name="Saturday Morning TV" enable="N" /><theme id="willie" name="Willie Nelson" enable="N" /><theme id="underdog" name="Saturday Morning TV" enable="N" /><theme id="willie" name="Willie Nelson" enable="N" /><theme id="fullenergy" name="Full Energy" thumb=""/><theme id="akon" name="AKON" /><theme id="ben10" name="Ben 10" /><theme id="botdf" name="Blood on the Dance Floor" cc_theme_id="botdf" /><theme id="bunny" name="It's Happy Bunny" enable="N" /><theme id="chowder" name="Chowder" enable="N" /><theme id="domo" name="Domo" enable="N" /><theme id="monkeytalk" name="SuperRica &amp; Rashy" enable="N" /><theme id="christmas" name="Holiday &amp; Seasonal" thumb="santa-stand.swf" /><theme id="animal" name="Lil' Petz" thumb="" /><theme id="bizmodels" name="Business Models" thumb="" /><theme id="sticklybiz" name="Stickly Business" thumb="" /><theme id="vietnam" name="Jungle Warfare" thumb="" /><theme id="monstermsh" name="Monsters Mayhem" thumb="" />`
+		var buffer = buffer.replace(/THEMEEXTRA/g, `\n${extrathemes}`);
 	}
 	res.setHeader("Content-Type", "application/zip");
-	fUtil.makeZip(0, "themelist.xml", buffer).then((b) => res.end(b));
+	fUtil.makeZip('0', "themelist.xml", buffer).then((b) => res.end(b));
+	return true;
+	*/
+	if (req.method != "POST" || url.path != "/goapi/getThemeList/") return;
+	res.setHeader("Content-Type", "application/zip");
+	fUtil.makeZip('0', "themelist.xml", themelist).then((b) => res.end(b));
 	return true;
 };
