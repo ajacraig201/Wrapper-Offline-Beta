2842a226739e2d69cc4b7952e6aacf584664a869
GoTest334 <60558304+GoTest334@users.noreply.github.com>
2022-03-07 16:41:47 -0500

    Update rpc.js

--- wrapper/misc/rpc.js
+++ wrapper/misc/rpc.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /***
  * discord rich presence
  */
@@ -5,6 +6,16 @@
 const version = process.env.WRAPPER_VER;
 const http = require("http");
 const rpc = process.env.drpc;
+=======
+const RPC = require("discord-rpc");
+const http = require("http");
+
+// Loads env.json for Wrapper version and build number
+const env = Object.assign(process.env,
+	require('../env'));
+// env.json variables
+let version = env.WRAPPER_VER;
+>>>>>>> parent of 625136aa (better)
 
 function setActivity(text) { // sets rpc activity
 	rpc.setActivity({
@@ -50,3 +61,31 @@ module.exports = function (req, res, url) {
 		}
 	}
 }
+<<<<<<< HEAD
+=======
+
+if (env.RPC == "y") {
+	rpc.on("ready", () => {
+		rpc.setActivity({
+			state: 'Waiting for RPC input...',
+			details: "Version " + version,
+			startTimestamp: new Date(),
+			largeImageKey: "icon",
+			largeImageText: "Wrapper: Offline",
+			smallImageKey: "Wrapper: Offline",
+			smallImagetext: "Wrapper: Offline",
+		});
+	});
+}
+// Connects RPC to app
+if (env.RPC == "y") {
+	try {
+		rpc.login({
+			clientId: "866340172874383370"
+		});
+		console.log('Rich presence is on!')
+	} catch (e) {
+		console.log(e);
+	}
+}
+>>>>>>> parent of 625136aa (better)
