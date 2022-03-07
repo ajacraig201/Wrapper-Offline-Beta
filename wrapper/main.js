8067899b4ab7a5dc39b10a292f323dab77e0c9a7
GoTest334 <60558304+GoTest334@users.noreply.github.com>
2022-03-07 17:18:06 -0500

    Revert "Revert "Update main.js""
    This reverts commit 6f87ec4ed0c237b2ba5a8f2e284a0c0496b020b5.

--- wrapper/main.js
+++ wrapper/main.js
@@ -23,13 +23,13 @@ if (process.env.RPC && process.env.RPC == "y") {
 	// get version number
 	const version = process.env.WRAPPER_VER;
 	const RPC = require("discord-rpc");
-	const process.env.rpc = new RPC.Client({
+	const process.env.drpc = new RPC.Client({
 		transport: "ipc"
 	});
 
 	// sets rpc activity when ready
-	process.env.rpc.on("ready", () => {
-		process.env.rpc.setActivity({
+	process.env.drpc.on("ready", () => {
+		process.env.drpc.setActivity({
 			details: `Version ${version}`,
 			startTimestamp: new Date(),
 			largeImageKey: "icon",
@@ -40,7 +40,7 @@ if (process.env.RPC && process.env.RPC == "y") {
 	});
 
 	// connect rpc to app
-	process.env.rpc
+	process.env.drpc
 		.login({ clientId: "866340172874383370" })
 		.catch((e) => console.log("RPC connection failed."));
 }
