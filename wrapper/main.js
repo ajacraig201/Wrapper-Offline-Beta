25662e61d711bbe22e52ed7e42a278ec21e3b465
GoTest334 <60558304+GoTest334@users.noreply.github.com>
2022-03-07 17:18:04 -0500

    Update main.js

--- wrapper/main.js
+++ wrapper/main.js
@@ -8,10 +8,13 @@ require("./server");
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> parent of a1625064 (Update main.js)
 =======
 >>>>>>> parent of fa63e302 (Update main.js)
+=======
+>>>>>>> parent of a1625064 (Update main.js)
 
 /**
  * rich presence
@@ -42,6 +45,7 @@ if (process.env.RPC && process.env.RPC == "y") {
 		.catch((e) => console.log("RPC connection failed."));
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> parent of 97ceef0c (Update main.js)
 <<<<<<< HEAD
@@ -53,3 +57,5 @@ if (process.env.RPC && process.env.RPC == "y") {
 >>>>>>> parent of fa63e302 (Update main.js)
 =======
 >>>>>>> parent of 97ceef0c (Update main.js)
+=======
+>>>>>>> parent of a1625064 (Update main.js)
